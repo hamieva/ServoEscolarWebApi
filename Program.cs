bf693ede0257ddcb07c6ca5397b89bd9a4789548
Hugo Amieva <amieva2@hotmail.com>
2023-06-24 19:12:23 -0600

    Permitir Swagger en tiempo de ejecución.

--- Program.cs
+++ Program.cs
@@ -65,11 +65,8 @@ namespace ServoEscolarWebApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseSwaggerUI(options =>
             {
